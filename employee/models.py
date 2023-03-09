b446a8f41b1873e50c8c9ba05d010b4f432c7b8b
Arslik <murmillo228@gmail.com>
2023-03-09 10:14:07 +0600

    Admin now can give a status

--- employee/models.py
+++ employee/models.py
@@ -3,13 +3,24 @@ from django.db import models
 
 class Employees(models.Model):
     employee_id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=20)
+    username = models.CharField(max_length=20, unique=True)
     firstname = models.CharField(max_length=20)
-    email = models.CharField(max_length=30)
-    phone_number = models.IntegerField()
-    chat_id = models.IntegerField()
-    status_name = models.CharField(max_length=20)
+    email = models.CharField(max_length=30, unique=True)
+    phone_number = models.CharField(max_length=20, unique=True)
+    chat_id = models.IntegerField(unique=True)
+    statuses = (
+        ('1', u'Granted'),
+        ('2', u'Pending'),
+        ('3', u'Blocked'),
+    )
+    status_name = models.CharField(max_length=20, choices=statuses, blank=True)
     curator = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.employee_id}--{self.username}"
+
+    class Meta:
+        db_table = "employee_employees"
 
 
 class Status(models.Model):
@@ -17,8 +28,5 @@ class Status(models.Model):
     employee_id = models.ForeignKey(Employees, on_delete=models.CASCADE)
     status_name = models.CharField(max_length=20)
 
-
-
-
-
-
+    class Meta:
+        db_table = "employee_status"
